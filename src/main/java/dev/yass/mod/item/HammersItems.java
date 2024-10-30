84a896e917fcd4e930af8290c99a669250ce5404
yaseno2186 <yaseno2186ss@gmail.com>
2024-10-30 19:56:22 +0100

    Added stats for Hammer

--- src/main/java/dev/yass/mod/item/HammersItems.java
+++ src/main/java/dev/yass/mod/item/HammersItems.java
@@ -1,8 +1,10 @@
 package dev.yass.mod.item;
 
 import dev.yass.mod.Morphine;
+import dev.yass.mod.item.custom.HammerItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.Tiers;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -11,7 +13,9 @@ public class HammersItems {
     public static final DeferredRegister.Items HAMMERS = DeferredRegister.createItems(Morphine.MOD_ID);
 
     public static final DeferredItem<Item> IRON_HAMMER = HAMMERS.register("iron_hammer",
-            () -> new Item(new Item.Properties().durability(1000)));
+            () -> new HammerItem(Tiers.IRON, new Item.Properties()
+                    .attributes(PickaxeItem.createAttributes(Tiers.IRON, 7F, -3.5f))
+                    .durability(1000)));
 
     public static void register(IEventBus eventBus) {
         HAMMERS.register(eventBus);
